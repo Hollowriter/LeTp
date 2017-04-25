cb376c49da9b1281d3d6adf56c9e4a1b5a456257
Hollowriter <deadgamergx2@hotmail.com.ar>
2017-04-24 21:56:20 -0300

    El primer ejercicio esta hecho, el segundo no

---
+++ LeTp/ConGoogle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace ConGoogle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombreDeLaCiudad;
+            string titulo;
+            Console.WriteLine("Escriba el nombre de una ciudad");
+            nombreDeLaCiudad = Console.ReadLine();
+            WebRequest req = WebRequest.Create("https://maps.googleapis.com/maps/api/geocode/json?address= " + nombreDeLaCiudad);
+
+            WebResponse respuesta = req.GetResponse();
+
+            Stream stream = respuesta.GetResponseStream();
+
+            StreamReader sr = new StreamReader(stream);
+
+            JObject data = JObject.Parse(sr.ReadToEnd());
+
+            titulo = (string)data["results"];
+            /*Como results es un array, no se puede convertir a string.
+             Y si le cambio la extencion por Array, no lo puede convertir a bytearray*/
+           
+            Console.WriteLine(titulo);
+
+           // Console.WriteLine(sr.ReadToEnd());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LeTp/LosLinks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace LosLinks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombreDeLaPelicula;
+            string titulo;
+            Console.WriteLine("Escriba el nombre de una pelicula");
+            nombreDeLaPelicula = Console.ReadLine();
+            try
+            {
+                WebRequest req = WebRequest.Create("http://www.omdbapi.com/?t= " + nombreDeLaPelicula);
+
+                WebResponse respuesta = req.GetResponse();
+
+                Stream stream = respuesta.GetResponseStream();
+
+                StreamReader sr = new StreamReader(stream);
+
+                JObject data = JObject.Parse(sr.ReadToEnd());
+
+                titulo = (string)data["Year"];
+            }
+            catch
+            {
+                titulo = "Ha ocurrido un error. Verifique su conexion, por favor.";
+            }
+
+            Console.WriteLine(titulo);
+
+            Console.ReadLine();
+        }
+    }
+}
